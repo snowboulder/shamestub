7c5de0a94d81d997c91ed631aefb6cdbd52401ae
Kevin Chao <kevin.chao@mail.utoronto.ca>
2016-06-21 17:00:34 -0400

    Fix minor changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { sessions: 'users/sessions' }, path: "auth",
+  devise_for :users, controllers: { sessions: "users/sessions" }, path: "auth",
               path_names: { sign_in: 'login', sign_out: 'logout' }
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  #root 'welcome#home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
